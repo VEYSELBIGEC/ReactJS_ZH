b227e3de8e7c6c6497b5c03c9bebd66911da23a5
Veysel BIGEC <veyselbgc@gmail.com>
2023-01-09 18:11:24 -0500

    03

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import HelloWorld from './components/01-hello-world/01-hello-world';
-import Jsx1 from './components/03-jsx/jsx1';
-import Jsx2 from './components/03-jsx/jsx2';
-import Jsx3 from './components/03-jsx/jsx3';
-import Jsx4 from './components/03-jsx/jsx4';
-import Jsx5 from './components/03-jsx/jsx5';
-import JsxPractise from './components/03-jsx/jsx-practise';
+import HelloWorld from "./components/01-hello-world/01-hello-world";
+import Jsx1 from "./components/03-jsx/jsx1";
+import Jsx2 from "./components/03-jsx/jsx2";
+import Jsx3 from "./components/03-jsx/jsx3";
+import Jsx4 from "./components/03-jsx/jsx4";
+import Jsx5 from "./components/03-jsx/jsx5";
+import JsxPractise from "./components/03-jsx/jsx-practise";
+import Style1 from "./components/04-styles/style1";
+import Style2 from "./components/04-styles/style2";
 
 function App() {
   return (
     <div className="App">
-      {/* <HelloWorld/>
+      {/* 
+      <HelloWorld/>
       <Jsx1/>
       <Jsx2/>
       <Jsx3/>
       <Jsx4/>
-      <Jsx5/> */}
+      <Jsx5/>
       <JsxPractise/>
+      <Style1/>
+       */}
+       <Style2/>
+       
     </div>
   );
 }
---
+++ src/components/04-styles/style2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Style2 = () => {
+  return (
+    <div>Style2</div>
+  )
+}
+
+export default Style2
