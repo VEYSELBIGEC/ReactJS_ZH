62eab3c991af0c13a06b1dd62fff4cc25fc52b5f
Veysel BIGEC <veyselbgc@gmail.com>
2023-01-10 23:58:16 -0500

    06

--- src/components/05-clock1/clock1.js
+++ src/components/05-clock1/clock1.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import "./clock1.scss"
+import React from "react";
+import "./clock1.scss";
 
 const Clock1 = () => {
   return (
-    <div className='clock-container'>
-        <div>20:11</div>
-        <div className='time'>
-            <div className='date'>August 13,2021</div>
-            <div className='day'>Friday Evening</div>
-        </div>
-        
+    <div className="clock-container">
+      <div className="time">20:11</div>
+      <div>
+        <div className="date">August 13,2021</div>
+        <div className="day">Friday Evening</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Clock1
+export default Clock1;
