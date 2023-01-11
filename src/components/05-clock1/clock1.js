4623cf42694ba10c51c8664f7e5eb9c049f074e8
Veysel BIGEC <veyselbgc@gmail.com>
2023-01-11 00:30:02 -0500

    07

--- src/components/05-clock1/clock1.js
+++ src/components/05-clock1/clock1.js
@@ -4,17 +4,33 @@ import moment from "moment";
 
 const Clock1 = () => {
 
+    const dateTime = moment();
+    const dateStr = dateTime.format("LL");
+    const timeStr = dateTime.format("HH:mm");
+    const dayStr = dateTime.format("dddd");
+    const hour = dateTime.format("HH");
+    let message = "";
 
-
-  const dateTime = moment();
+    if(hour>=6 && hour<11){
+        message="Morning";
+    }
+    else if(hour>=11 && hour<17){
+        message="Afternoon";
+    }
+    else if(hour>=17 && hour<22){
+        message="Evening";
+    }
+    else{
+        message="Night";
+    }
 
 
   return (
     <div className="clock-container">
-      <div className="time">20:11</div>
+      <div className="time">{timeStr}</div>
       <div>
-        <div className="date">August 13,2021</div>
-        <div className="day">Friday Evening</div>
+        <div className="date">{dateStr}</div>
+        <div className="day">{dayStr} {message}</div>
       </div>
     </div>
   );
