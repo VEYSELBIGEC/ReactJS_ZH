d5cf4410ce7542147603857db28b850ad2fbcd0c
Veysel BIGEC <veyselbgc@gmail.com>
2023-01-15 23:27:14 -0500

    12

--- src/components/08-images/image1.js
+++ src/components/08-images/image1.js
@@ -4,7 +4,9 @@ import image1Src from "../../assets/img/image1.jpg" ;
 const Image1 = () => {
   return (
     <div>
+        {/* If images are in public file, we import like this!*/}
         <img src="logo512.png"/>
+        {/* If images are in src file, we import like two different ways!*/}
         <img src={image1Src}/>
         <img src={require("../../assets/img/image2.jpg")}/>
     </div>
