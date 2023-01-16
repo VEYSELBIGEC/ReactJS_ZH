c089cec4efca5fe9f9d57ba46c396507ecf93043
Veysel BIGEC <veyselbgc@gmail.com>
2023-01-15 23:24:05 -0500

    12

--- src/components/08-images/image1.js
+++ src/components/08-images/image1.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import image1Src from "../../assets/img/image1.jpg" ;
 
 const Image1 = () => {
   return (
     <div>
         <img src="logo512.png"/>
+        <img src={image1Src}/>
+        <img src={require("../../assets/img/image2.jpg")}/>
     </div>
   )
 }
